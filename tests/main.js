ee99efac71ab343592e124e2aa58527d4357413c
Daniel Suo <danielsuo@gmail.com>
2014-06-24 11:53:10 -0400

    Fixing tests config baseUrl for requirejs

--- tests/main.js
+++ tests/main.js
@@ -1,5 +1,5 @@
 require.config({
-  baseUrl: '/',
+  baseUrl: '../',
   paths: {
     'fallback': 'fallback'
   }
@@ -12,5 +12,5 @@ require(['fallback'], function(fallback) {
     //the module value for "helper/util".
 
     console.log(fallback.shouldShow);
-    fallback.show('/tests/streetview', {});
+    fallback.show('../tests/streetview', {});
 });
